6d9d5d6a596f0ac09dfbd79df963c8535eb84128
MaliugaSergii <shtirliy@gmail.com>
2017-12-09 15:05:31 +0200

    carousel coded v 0.1

--- dev/js/common.js
+++ dev/js/common.js
@@ -44,35 +44,104 @@ $(document).ready(function () {
 
 
     //modals 
-    
-    // Click function for show the Modal
+    (function modal() {
+        // Click function for show the Modal
+        $(".choice").on("click", function (e) {
+            e.preventDefault();
+            $(".overlay").addClass("active");
+            $("body").addClass("oh");
 
-    $(".choice").on("click", function(e) {
-        e.preventDefault();
-        $(".overlay").addClass("active");
-        $("body").addClass("oh");
-        
-    });
+        });
 
-    // Function for close the Modal
+        // Function for close the Modal
 
-    function closeModal() {
-        $(".overlay").removeClass("active");
-        $("body").removeClass("oh");
-    }
-
-    // Call the closeModal function on the clicks/keyboard
-
-    $(".close, .overlay").on("click", function () {
-        closeModal();
-    });
-
-    $(document).keyup(function (e) {
-        if (e.keyCode == 27) {
-            closeModal();
+        function closeModal() {
+            $(".overlay").removeClass("active");
+            $("body").removeClass("oh");
         }
-    });
 
+        // Call the closeModal function on the clicks/keyboard
+
+        $(".close, .overlay").on("click", function () {
+            closeModal();
+        });
+
+        $(document).keyup(function (e) {
+            if (e.keyCode == 27) {
+                closeModal();
+            }
+        });
+    })();
+
+    //carousel function
+
+    (function carousel() {
+        let corousel_container = $(".corousel_container"),
+            carousel_item = corousel_container.find(".item"),
+            item_amount = carousel_item.length;
+
+        function move_slide(event) {
+            event.preventDefault();
+            let scrolled_position = +corousel_container.attr("data-scrolled"),
+                corousel_container_width = corousel_container.outerWidth(),
+                carousel_item_width = carousel_item.outerWidth(),
+                total_width_of_items = carousel_item_width * item_amount,
+                direction = $(this).data("direction"),
+                item_translation;
+
+            if (direction) {
+                if (scrolled_position == item_amount) {
+                    scrolled_position = 0;
+                } else {
+                    scrolled_position++;
+                }
+
+            } else {
+                if (scrolled_position == 0) {
+                    scrolled_position = item_amount;
+                } else {
+                    scrolled_position--;
+                }
+
+            };
+
+            corousel_container.attr("data-scrolled", scrolled_position);
+
+            item_translation = scrolled_position * carousel_item_width * -1;
+            carousel_item.each(function () {
+                $(this).eq(0).css('transform', 'translateX(' + item_translation + 'px)');
+            });
+        }
+
+        //        $(".corousel_left").on("click", function (event) {
+        //            event = event || window.event;
+        //            event.preventDefault();
+        //            move_slide(0);
+        //        });
+        //        
+        //        $(".corousel_right").on("click", function (event) {
+        //            event = event || window.event;
+        //            event.preventDefault();
+        //            move_slide(1);
+        //        });
+
+        $(".corousel_left, .corousel_right").on("click", move_slide);
+
+        function setTransitionToItems() {
+            let scrolled_position = +corousel_container.attr("data-scrolled"),
+                carousel_item_width = carousel_item.outerWidth(),
+                item_translation;
+
+            item_translation = scrolled_position * carousel_item_width * -1;
+            carousel_item.each(function () {
+                $(this).eq(0).css('transform', 'translateX(' + item_translation + 'px)');
+            });
+        }
+
+        $(window).on("resize", setTransitionToItems);
+        setTransitionToItems();
+
+    })();
 
 });
 
